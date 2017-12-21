532d18793f43b50c224aafee1b4bb1c6fc89d8c0
xuy <xuyff08@gmail.com>
2017-12-22 06:14:49 +0800

    调通微信

--- wm/views.py
+++ wm/views.py
@@ -12,12 +12,13 @@ from django.http import HttpResponse
 def index(request):
     if request.method == "GET":
         # 接收微信服务器get请求发过来的参数
-        signature = request.GET.get('signature', None)
-        timestamp = request.GET.get('timestamp', None)
-        nonce = request.GET.get('nonce', None)
-        echostr = request.GET.get('echostr', None)
+        signature = request.GET.get('signature')
+        timestamp = request.GET.get('timestamp')
+        nonce = request.GET.get('nonce')
+        echostr = request.GET.get('echostr')
         # 服务器配置中的token
         token = 'xuy'
+
         # 把参数放到list中排序后合成一个字符串，再用sha1加密得到新的字符串与微信发来的signature对比，如果相同就返回echostr给服务器，校验通过
         hashlist = [token, timestamp, nonce]
         hashlist.sort()
